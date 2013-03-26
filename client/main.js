e0cc57ba0fd8849ded4aae970121781a6cfbfdd8
Derek Thurn <derek@thurn.ca>
2013-03-26 04:29:01 -0700

    add normalize.css, working on reactive menu

--- client/main.js
+++ client/main.js
@@ -66,7 +66,7 @@
     window.App = (function()
     {
 
-        var _init = false, app = { };
+        var _init = false, app = { },
 
             nav_open = false,
 
@@ -129,9 +129,6 @@
 
             // open nav with main "nav" button
             document.getElementById('nav-open-btn').addEventListener('click', app.toggleNav, false);
-
-            // close nav with main "close" button
-            document.getElementById('nav-close-btn').addEventListener('click', app.toggleNav, false);
 
             // close nav by touching the partial off-screen content
             document.addEventListener('click', function(e)
