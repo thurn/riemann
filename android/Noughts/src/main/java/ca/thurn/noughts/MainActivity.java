b22d8470a6163db11e81ff3e64439067d7235288
Derek Thurn <derek@fake.email>
2013-07-08 06:53:01 -0700

    android app that opens url in webview

--- android/Noughts/src/main/java/ca/thurn/noughts/MainActivity.java
+++ android/Noughts/src/main/java/ca/thurn/noughts/MainActivity.java
@@ -2,22 +2,24 @@ package ca.thurn.noughts;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        WebView browser = (WebView)findViewById(R.id.browser);
+        browser.setWebChromeClient(new WebChromeClient());
+        browser.setWebViewClient(new WebViewClient());
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.setVerticalScrollBarEnabled(false);
+        browser.setHorizontalScrollBarEnabled(false);
+        browser.loadUrl("https://noughts.meteor.com");
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
